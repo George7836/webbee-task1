b304482b81c15663b51c22bd2d2370c78024eda0
George Aliev <gera08012329@gmail.com>
2023-07-03 18:50:03 +0300

    fix: add / to the routes

--- js/router.js
+++ js/router.js
@@ -2,10 +2,10 @@ import { loadMap } from "./map.js";
 import { setValuesInDOM } from "./timer.js";
 
 const routes = {
-    'webbee-task1/index.html': '/pages/activity.html',
-    'webbee-task1/': '/pages/activity.html',
-    'webbee-task1/map': '/pages/map.html',
-    'webbee-task1/time': '/pages/time.html'
+    '/webbee-task1/index.html': '/pages/activity.html',
+    '/webbee-task1/': '/pages/activity.html',
+    '/webbee-task1/map': '/pages/map.html',
+    '/webbee-task1/time': '/pages/time.html'
 }
 
 window.addEventListener('click', (e) => {
